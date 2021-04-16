f7cb70eefb7c5cb136c03bcb96160417338dc0ec
Grogan-Kaylor <agrogan@UMICH.EDU>
2021-04-16 16:51:13 -0400

    first commit

---
+++ mapping/plotly-globe-test.R
@@ -0,0 +1,19 @@
+library(plotly)
+
+g <- list(# projection = list(type = 'robinson'),
+          projection = list(type = 'orthographic',
+                            rotation = list(lon = 0,
+                                            lat = 0,
+                                            roll = 0)))
+
+plot_ly(type = "scattermapbox") %>%
+  layout(mapbox= list(
+    style = "white-bg",
+    sourcetype = 'raster',
+    zoom = .5,
+    center = list(lon = 0,
+                  lat= 0),
+    layers = list(list(
+      below = 'traces',
+      sourcetype = "raster",
+      source = list("https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}")))))
