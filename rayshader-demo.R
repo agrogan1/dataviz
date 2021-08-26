c8cc6f1282dd6f11ac70e443589343b44981e4a8
Andrew Grogan-Kaylor <agrogan@0587351086.wireless.umich.net>
2021-08-26 17:31:20 -0400

    first commit

---
+++ rayshader-demo.R
@@ -0,0 +1,30 @@
+# rayshader demo
+
+library(ggplot2)
+
+library(ggthemes)
+
+library(rgl)
+
+library(rayshader)
+
+x <- rnorm(100, 0, 10)
+
+y <- rnorm(100, 0, 10)
+
+mydata <- data.frame(x,y)
+
+p1 <- ggplot(data = mydata,
+             aes(x = x,
+                 y = y,
+                 color = x * y)) +
+  geom_point() +
+  labs(title = "A Scatterplot") +
+  scale_color_viridis_c()
+
+p1
+
+rayshader::plot_gg(p1, 
+                   zoom = .8) 
+
+# rglwidget()
