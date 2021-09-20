7a800b21e6a8c0c5d9584aadace989ac2a472ae1
Grogan-Kaylor <agrogan@UMICH.EDU>
2021-09-20 11:25:02 -0400

    first commit

---
+++ SW672/spotify-lab/wrangle-data.R
@@ -0,0 +1,36 @@
+# Wrangle Data For Spotify Lab
+# data are from https://github.com/rfordatascience/tidytuesday/blob/master/data/2020/2020-01-21/readme.md
+
+# get data
+
+spotify_songs <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-01-21/spotify_songs.csv')
+
+# create song date variable
+
+spotify_songs$song_date <- as.Date(spotify_songs$track_album_release_date)
+
+# save data
+
+save(spotify_songs, file = "./SW672/spotify-lab/spotify.RData")
+
+# demonstration graph
+
+library(ggplot2) # call ggplot 
+
+library(ggthemes) # better color palettes
+
+ggplot(spotify_songs,
+       aes(x = song_date,
+           y = energy,
+           color = playlist_genre)) + 
+  geom_point() +
+  scale_color_pander(name = "Genre") +
+  theme_minimal() +
+  labs(title = "Song Energy by Date and Genre",
+       subtitle = "Spotify Data",
+       x = "Date",
+       y = "Spotify Measure of Song Energy")
+
+
+
+
