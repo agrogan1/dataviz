34cb4e40508b1f3372a7f4e1bc6efb2bed4a35d3
Andrew Grogan-Kaylor <agrogan@dhcp-17-132.ssw.umich.edu>
2021-12-22 12:41:22 -0500

    tweaks

--- mapping/plotly-map-test.R
+++ mapping/plotly-map-test.R
@@ -1,10 +1,12 @@
+# plotly map test
+
 library(plotly)
 
 g <- list(# projection = list(type = 'robinson'),
-          projection = list(type = 'orthographic',
-                            rotation = list(lon = 0,
-                                            lat = 0,
-                                            roll = 0)))
+  projection = list(type = 'orthographic',
+                    rotation = list(lon = 0,
+                                    lat = 0,
+                                    roll = 0)))
 
 plot_ly(type = "scattermapbox") %>%
   layout(mapbox= list(
