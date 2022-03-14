3a3bed46321c969aa4431265f91fc36968054aed
Andrew Grogan-Kaylor <agrogan@Andrews-MacBook-Air.local>
2022-03-14 14:01:41 -0400

    first major commit

---
+++ mapping/global-shapefile-test.R
@@ -0,0 +1,61 @@
+# testing out writing a global shapefile
+
+library(maps) # maps
+
+library(maptools) # maptools
+
+library(countrycode) # manipulate country names and codes
+
+library(dplyr) # data wrangling
+
+data(wrld_simpl) # world simple data
+
+global_data <- st_as_sf(wrld_simpl) # make an sf dataset
+
+# write sf to shapefile
+
+st_write(global_data, "mapping/shapefiles/wrld_simpl/wrld_simpl.shp")
+
+# get MICS countries
+
+country <- c("Afghanistan", "Algeria",  "Argentina",  
+             "Bangladesh",  "Barbados",  "Belarus",  
+             "Belize",  "Benin",  "Bosnia and Herzegovina",  
+             "Cameroon",  "Central African Republic",  "Chad",  
+             "Democratic Republic of the Congo",  
+             "Republic of the Congo",  
+             "Costa Rica",  "Cote d'Ivoire",  "Dominican Republic",  
+             "El Salvador",  "Eswatini",  "Ghana",  
+             "Guinea",  "Guinea Bissau",  "Guyana",  
+             "Iraq",  "Jamaica",  "Kazakhstan",  
+             "Kenya",  "Kosovo",  "Kyrgyzstan",  
+             "Laos", "Macedonia",  "Madagascar",  
+             "Malawi",  "Mali",  "Mauritania",  
+             "Mexico",  "Moldova",  "Mongolia",  
+             "Montenegro",  "Nepal",  "Nigeria",  
+             "Pakistan",  "Panama",  "Paraguay",  
+             "Sao Tome and Principe",  "Senegal",  "Serbia",  
+             "Sierra Leone",  "Somalia",  "St. Lucia",  
+             "State of Palestine",  "Suriname",  "Thailand",  
+             "The Gambia",  "Togo",  "Trinidad and Tobago",  
+             "Tunisia",  "Turkmenistan",  "Ukraine",  
+             "Uruguay",  "Vietnam",  "Zimbabwe")
+
+# convert to ISO3
+
+country_iso <- countrycode(country, 
+                           'country.name', 
+                           'iso3c')
+
+# MICS is an sf object that is subset of global_data
+
+MICS <- global_data %>% 
+  filter(ISO3 %in% country_iso)
+
+st_write(MICS, "mapping/shapefiles/MICS/MICS.shp")
+
+
+
+
+
+
